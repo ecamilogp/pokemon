70a84d9f57c2df1603e2cbaf0809233e7fdf295e
camilo <ecgarciap@slabcode.com>
2025-01-17 12:55:48 -0500

    +´+´+´+´

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { fileURLToPath } from 'node:url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import { quasar } from '@quasar/vite-plugin';
+import { fileURLToPath } from 'url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
