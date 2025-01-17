0849baeee7b88dd7883f7b4afa31ae65aae64e3f
camilo <ecgarciap@slabcode.com>
2025-01-17 13:00:31 -0500

    [[{{[][[

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
+import { fileURLToPath } from 'node:url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import { quasar } from '@quasar/vite-plugin';
-import { fileURLToPath } from 'url';
+import { quasar, transformAssetUrls } from '@quasar/vite-plugin';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
+    vue({
+      template: { transformAssetUrls },
+    }),
 
     // @quasar/plugin-vite options list:
     // https://github.com/quasarframework/quasar/blob/dev/vite-plugin/index.d.ts
