cf26941e7ac753243b58803962b1f56a63edecd6
camilo <ecgarciap@slabcode.com>
2025-01-17 12:34:34 -0500

    ......

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { quasar, transformAssetUrls } from '@quasar/vite-plugin';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'pokemon',
   plugins: [
     vue({
       template: { transformAssetUrls },
